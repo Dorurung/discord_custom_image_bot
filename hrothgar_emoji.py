4794439865ab6539f5c79418c111d2837d441e7a
Dorurung <james580309@gmail.com>
2022-04-20 15:30:42 +0900

    Several changes and added features

--- hrothgar_emoji.py
+++ hrothgar_emoji.py
@@ -9,6 +9,8 @@ import os
 import random
 import tweepy
 import re
+import hashlib
+import asyncio
 from collections import defaultdict
 
 client = discord.Client()
@@ -16,6 +18,7 @@ client = discord.Client()
 keyword_id={}
 id_image={}
 blacklist = defaultdict(lambda: [])
+blacklistall = []
 
 with open('tokens.txt', 'r') as f:
     exec(f.read())
@@ -94,6 +97,9 @@ def add_image(message, keyword):
 
 archive = ''
 
+ffxiv = ['nsfw']
+debu = ['인간 데부', '인외-슈잉 데부']
+
 @client.event
 async def on_ready():
     global archive
@@ -106,10 +112,12 @@ async def on_reaction_add(reaction, user):
     if user == client.user:
         return
     if reaction.message.channel.name != '아카이브':
-        if reaction.emoji.name == 'timberwolf36':
-            if reaction.count == 1:
+        print(reaction.emoji)
+        if reaction.emoji == '⭐':
+            if reaction.count == 3:
                 e = discord.Embed(description=f'{reaction.message.content}\n\n{reaction.message.channel.name}에서 {str(reaction.message.created_at)}에 보냄')
                 e.set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url)
+                archive = discord.utils.get(reaction.message.guild.channels, name='아카이브')
                 await archive.send(embed=e)
 
 @client.event
@@ -117,7 +125,7 @@ async def on_message(message):
     if message.author == client.user:
     	return
 
-    if message.content in keyword_id.keys() and not message.content in blacklist[message.author]:
+    if message.content in keyword_id.keys() and not message.content in blacklist[message.author] and not message.author in blacklistall:
         image_name = id_image[keyword_id[message.content]]
         image_url = 'image/' + image_name
         extension = '.' + image_name.split('.')[-1]
@@ -188,6 +196,14 @@ async def on_message(message):
         else:
             await message.channel.send('없는 이미지ID래!')
     
+    if message.content.startswith('!전체차단해제'):
+        blacklistall.remove(message.author)
+        await message.channel.send(f'{message.author}는 감정표현을 할 수 있게 되었어!')
+    
+    elif message.content.startswith('!전체차단'):
+        blacklistall.append(message.author)
+        await message.channel.send(f'{message.author}는 감정표현을 할 수 없게 되었어!')
+
     if message.content.startswith('!블랙리스트등록') or message.content.startswith('!블랙리스트추가'):
         array = message.content.split(' ')[1:]
         blacklist[message.author].extend(array)
@@ -205,14 +221,24 @@ async def on_message(message):
         image_url = 'nsfw/' + filelist[random.randrange(0, len(filelist))]
         await message.channel.send(file=discord.File(image_url))
 
-    if message.channel.name == 'nsfw' and message.author != '미니쿠다':
+    if message.content.startswith('!랜덤데부'):
+        filelist = os.listdir('debu')
+        image_url = 'debu/' + filelist[random.randrange(0, len(filelist))]
+        await message.channel.send(file=discord.File(image_url))
+
+    if (message.channel.name in ffxiv or str(message.channel.category) in debu) and message.author != '미니쿠다':
+        if message.channel.name in ffxiv:
+            savedir = 'nsfw'
+        elif str(message.channel.category) in debu:
+            savedir = 'debu'
         try:
-            r = requests.get(message.attachments[0].url, stream=True)
-            extension = mimetypes.guess_extension(r.headers.get('content-type', '').split(';')[0])
-            filenum = len(os.listdir('nsfw'))
-            with open(f'nsfw/{filenum}{extension}', 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-            await message.add_reaction('✅')
+            for i in message.attachments:
+                r = requests.get(i.url, stream=True)
+                extension = mimetypes.guess_extension(r.headers.get('content-type', '').split(';')[0])
+                filenum = len(os.listdir(savedir))
+                with open(f'{savedir}/{filenum}{extension}', 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
+                await message.add_reaction('✅')
         except:
             pass
         if 'https://twitter.com/' in message.content:
@@ -224,17 +250,83 @@ async def on_message(message):
                     url = media['media_url']
                     r = requests.get(url, stream=True)
                     extension = mimetypes.guess_extension(r.headers.get('content-type', '').split(';')[0])
-                    filenum = len(os.listdir('nsfw'))
-                    with open(f'nsfw/{filenum}{extension}', 'wb') as f:
+                    filenum = len(os.listdir(savedir))
+                    with open(f'{savedir}/{filenum}{extension}', 'wb') as f:
                         shutil.copyfileobj(r.raw, f)
             except:
                 url = lookup[0].entities['media'][0]['media_url']
                 r = requests.get(url, stream=True)
                 extension = mimetypes.guess_extension(r.headers.get('content-type', '').split(';')[0])
-                filenum = len(os.listdir('nsfw'))
-                with open(f'nsfw/{filenum}{extension}', 'wb') as f:
+                filenum = len(os.listdir(savedir))
+                with open(f'{savedir}/{filenum}{extension}', 'wb') as f:
                     shutil.copyfileobj(r.raw, f)
             await message.add_reaction('✅')
+    
+    if message.content.startswith('!배틀'):
+        _, name1, name2 = message.content.split(' ')
+
+        name1 = '**' + name1 + '**'
+        name2 = '**' + name2 + '**'
+
+        seed = (hashlib.md5(name1.encode()).hexdigest(), hashlib.md5(name2.encode()).hexdigest())
+        stat = [{}, {}]
+        for i, s in enumerate(stat):
+            s['atk'] = int(seed[i][:2], 16) 
+            s['def'] = int(seed[i][2:4], 16)
+            s['hit'] = int(seed[i][4:6], 16)
+            s['agi'] = int(seed[i][6:8], 16)
+            s['maxhp'] = int(int(seed[i][8:11], 16) / 4) + 128
+            s['hp'] = s['maxhp']
+        
+        def hpbar(stat, name1, name2):
+            p1percent = int(stat[0]['hp'] / stat[0]['maxhp'] * 10 + 0.99)
+            p1box = '□'*min((10-p1percent), 10) + '■'*p1percent
+            p2percent = int(stat[1]['hp'] / stat[1]['maxhp'] * 10 + 0.99)
+            p2box = '■'*p2percent + '□'*min((10-p2percent), 10) 
+            return f'\n[{name1}]{p1box}vs{p2box}[{name2}]'
+
+        def sigmoid(x):
+            return 1 / (1 +np.exp(-x))
+
+        sendmessage = await message.channel.send(f'''{name1}과 {name2}의 배틀!
+{name1}) ATK: {stat[0]['atk']}, DEF: {stat[0]['def']}, HIT: {stat[0]['hit']}, AGI: {stat[0]['agi']}, HP: {stat[0]['hp']}
+{name2}) ATK: {stat[1]['atk']}, DEF: {stat[1]['def']}, HIT: {stat[1]['hit']}, AGI: {stat[1]['agi']}, HP: {stat[1]['hp']}
+[{name1}]->[{name2}] 명중률: {int(sigmoid((stat[0]['hit'] - stat[1]['agi'])/128)*100)}%, 공격력: {int(sigmoid((stat[0]['atk'] - stat[1]['def'])/128)*512)}~{int(sigmoid((stat[0]['atk'] - stat[1]['def'])/128)*512)+64}
+[{name2}]->[{name1}] 명중률: {int(sigmoid((stat[1]['hit'] - stat[0]['agi'])/128)*100)}%, 공격력: {int(sigmoid((stat[1]['atk'] - stat[0]['def'])/128)*512)}~{int(sigmoid((stat[1]['atk'] - stat[0]['def'])/128)*512)+64}
+{name1 if stat[0]['agi'] > stat[1]['agi'] else name2}의 선공''')
+        
+
+        name1turn = True if stat[0]['agi'] > stat[1]['agi'] else False
+        final_string = sendmessage.content
+        while stat[0]['hp'] > 0 and stat[1]['hp'] > 0:
+            if name1turn:
+                hit_percent = sigmoid((stat[0]['hit'] - stat[1]['agi'])/128)
+                if random.random() <= hit_percent:
+                    dmg = int(sigmoid((stat[0]['atk'] - stat[1]['def'])/128)*512) + random.randrange(0, 64)
+                    string = f'\n[{name1}]의 공격! [{name2}]는 {dmg}의 피해를 입었다!'
+                    stat[1]['hp'] -= dmg
+                else:
+                    string = f'\n[{name1}]의 공격! [{name2}]는 공격을 회피했다!'
+                name1turn = False
+            else:
+                hit_percent = sigmoid((stat[1]['hit'] - stat[0]['agi'])/128)
+                if random.random() <= hit_percent:
+                    dmg = int(sigmoid((stat[1]['atk'] - stat[0]['def'])/128)*512) + random.randrange(0, 64)
+                    string = f'\n[{name2}]의 공격! [{name1}]는 {dmg}의 피해를 입었다!'
+                    stat[0]['hp'] -= dmg
+                else:
+                    string = f'\n[{name2}]의 공격! [{name1}]는 공격을 회피했다!'
+                name1turn = True
+            final_string = final_string + string
+            await sendmessage.edit(content=final_string+hpbar(stat, name1, name2))
+            await asyncio.sleep(2)
+        if stat[1]['hp'] < 0:
+            string = f'\n[{name2}]가 쓰러졌다. [{name1}]의 승리!'
+        else:
+            string = f'\n[{name1}]가 쓰러졌다. [{name2}]의 승리!'
+        final_string = final_string + string
+        await sendmessage.edit(content=final_string+hpbar(stat, name1, name2))
+
 '''
 @client.event
 async def on_message_edit(before, message):
